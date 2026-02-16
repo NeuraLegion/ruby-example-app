34ec9e510044b840f8561c1780ce99715d7e482d
Viachaslau <slava@tssbox.com>
2026-02-16 17:09:09 +0400

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/post-users-json.test.ts
@@ -0,0 +1,48 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('POST /users.json', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['csrf', 'bopla', 'xss', 'sqli', 'email_injection'],
+      attackParamLocations: [AttackParamLocation.BODY],
+      starMetadata: {
+        code_source: 'NeuraLegion/ruby-example-app:chore/ci',
+        databases: ['PostgreSQL'],
+        user_roles: ['admin']
+      },
+      poolSize: +process.env.SECTESTER_SCAN_POOL_SIZE || undefined
+    })
+    .setFailFast(false)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.POST,
+      url: `${baseUrl}/users.json`,
+      body: {
+        user: {
+          email: 'example@example.com',
+          password: 'password123',
+          password_digest: '$2a$12$KIXQ1Y1rZ1u1Q1Q1Q1Q1QO',
+          admin: false
+        }
+      },
+      headers: { 'Content-Type': 'application/json' }
+    });
+});
